9b5d728043c199a82c51b191e9a912d3151f5ad3
Vladyslav <arteezysolo@gmail.com>
2023-09-08 09:50:59 +0300

    123

--- config/wdio.android.app.conf.ts
+++ config/wdio.android.app.conf.ts
@@ -1,6 +1,6 @@
 import { join } from 'path';
 import config from './wdio.shared.local.appium.conf';
-
+let APP_ANDROID = 'test.apk';
 // ============
 // Specs
 // ============
@@ -15,21 +15,16 @@ config.specs = [
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [
     {
-        // The defaults you need to have in your config
         platformName: 'Android',
-        maxInstances: 1,
-        // For W3C the appium capabilities need to have an extension prefix
-        // http://appium.io/docs/en/writing-running-appium/caps/
-        // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_3_10.0',
-        'appium:platformVersion': '10.0',
-        'appium:orientation': 'PORTRAIT',
-        'appium:automationName': 'UiAutomator2',
-        // The path to the app
-        'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.4.0.apk'),
-        // @ts-ignore
-        'appium:appWaitActivity': 'com.wdiodemoapp.MainActivity',
-        'appium:newCommandTimeout': 240,
+        'appium:platformVersion': '12', // This is `appium:` for all Appium Capabilities which can be found here
+        'appium:deviceName': 'emulator-5554',
+        'appium:app': join('./apk/', APP_ANDROID || ''), // The path to the app
+        'appium:automationName': 'UiAutomator2', // Will be mandatory, see https://github.com/appium/appium/releases/tag/v1.13.0
+        //'appium:autoGrantPermissions': true,
+        //'appium:noReset': true, // Reset strategies differ per platform, see http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
+        //'appium:gpsEnabled': true,
+        //"appium:autoWebview": true, //! IMPORTANT
+        //'appium:newCommandTimeout': '300',
     },
 ];
 
